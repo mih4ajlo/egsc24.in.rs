9f5e2093b9a6fc728af038936088be138725b4f7
Mihajlo Andjelic <mihajlo.andjelic@jolausa.com>
2024-08-08 20:58:51 +0200

    tables

--- app/participants/page.jsx
+++ app/participants/page.jsx
@@ -1,28 +1,64 @@
 import React from "react";
 import { createClient } from "@/utils/supabase/server";
 
-const notToDisplay = ["created_at", "id", "e_mail"];
+const notToDisplay = ["created_at", "id", "e_mail", "faculty","degree"];
 
-const bodyJSX = (participants) =>
-  participants?.map((partic) => {
-    return (
+const maping = {
+  "first_name": "Name", 
+  "last_name":"Last name",
+  "category":"Category",
+  "rating":"Rating",
+  "university": "University",
+  "country":"Country"
+}
+
+
+const bodyJSX = (participants) =>{
+
+  const arrLa = [];
+   participants?.forEach(( partic, ind) => {  
+    
+    let tempPar = Object.entries(partic);
+    
+    tempPar = [...[['','#']],...tempPar]
+    
+    
+
+    const tem =  (
       <tr>
-        {Object.entries(partic).map(
-          (el) => {return notToDisplay.indexOf(el[0]) == -1 ? (<td>{el[1]}</td>) : null}
-        )}
+        {tempPar.map(
+          (el) => {
+            if(el[1]==='#') {
+              
+              return <td>{ind +1}</td>;}
+              return notToDisplay.indexOf(el[0]) == -1 ? (<td>{el[1]}</td>) : null},
+              ind
+            )}
       </tr>
     );
+    arrLa.push(tem);
   });
+
+  return arrLa;
+}
 
 const headJSX = (participants) => {
 
   if (! Array.isArray(participants) ) {
-    return <>prazno</>;
+    return <>no entries</>;
   }
+
+  let tempPar = Object.entries(participants[0])
+
+  tempPar = [...[['#','']],...tempPar]
+
   return (
       <tr>
-        {Object.entries(participants[0]).map(
-          (el) => {return notToDisplay.indexOf(el[0]) == -1 ? (<td>{el[0]}</td>) : null}
+        {tempPar.map(
+          (el, i) => {
+            if(el[0]==='#')return <th>#</th>;
+            return notToDisplay.indexOf(el[0]) == -1 ? (<th>{ maping[el[0]] }</th>) : null
+          }
         )}
       </tr>
     );
@@ -31,6 +67,7 @@ const headJSX = (participants) => {
 
 const renderTable = (data) => {
 
+  
 
   let headJ = headJSX(data);
   let bodJ =  bodyJSX(data);
